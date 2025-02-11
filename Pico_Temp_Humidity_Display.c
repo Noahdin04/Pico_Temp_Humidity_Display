6a964faeb4378d13de252b33377a9d695867c1d1
Noahdin04 <92384217+Noahdin04@users.noreply.github.com>
2025-02-10 21:26:18 -0600

    Build 0.03
    Turns on and off general purpose io pin 0 every second

--- Pico_Temp_Humidity_Display.c
+++ Pico_Temp_Humidity_Display.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
 int main()
 {
-    stdio_init_all();
+    gpio_init(0);
+    gpio_set_dir(0, GPIO_OUT);
 
-    while (true) {
-        printf("Hello, world!\n");
+    while(1) {
+        gpio_put(0, 1);
+        sleep_ms(1000);
+        gpio_put(0,0);
         sleep_ms(1000);
     }
 }
